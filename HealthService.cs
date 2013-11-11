97cfbae3ab2a70e99e590befb5b911d558a435b6
mavnn <michael@mavnn.co.uk>
2013-11-11 10:59:25 +0000

    Turn health service into topshelf service

---
+++ HealthService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using Riemann;
+
+namespace RiemannHealth
+{
+    public class HealthService
+    {
+        public bool Cancel { get; set; }
+
+        public string Host { get; private set; }
+        public int Port { get; private set; }
+        public bool IncludeGC { get; private set; }
+
+        public HealthService(string host, int port = 5555, bool includeGc = false)
+        {
+            Cancel = false;
+            Host = host;
+            Port = port;
+            IncludeGC = includeGc;
+        }
+
+        private HealthService() { }
+
+        public void Start()
+        {
+            Cancel = false;
+            var client = new Client(Host, Port);
+
+            var reporters = Health.Reporters(IncludeGC)
+                .ToList();
+
+            while (!Cancel)
+            {
+                foreach (var reporter in reporters)
+                {
+                    string description;
+                    float value;
+
+                    if (reporter.TryGetValue(out description, out value))
+                    {
+                        string state;
+                        if (value < reporter.WarnThreshold)
+                        {
+                            state = "ok";
+                        }
+                        else if (value < reporter.CriticalThreshold)
+                        {
+                            state = "warning";
+                        }
+                        else
+                        {
+                            state = "critical";
+                        }
+                        client.SendEvent(reporter.Name, state, description, value, 1);
+                    }
+                }
+                Thread.Sleep(TimeSpan.FromSeconds(1.0));
+            }
+        }
+
+        public void Stop()
+        {
+            Cancel = true;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,69 +1,28 @@
 ﻿using System;
 using System.Configuration;
-using System.Linq;
-using System.Threading;
-using Riemann;
+using Topshelf;
 
 namespace RiemannHealth {
 	public class Program {
 		public static void Main(string[] args) {
-			string hostname;
-			ushort port;
-			bool includeGCStats;
-			switch (args.Length) {
-				case 0:
-					var appSettings = ConfigurationManager.AppSettings;
-					hostname = appSettings["RiemannHost"];
-					port = UInt16.Parse(appSettings["RiemannPort"]);
-					includeGCStats = Boolean.Parse(appSettings["IncludeGCstats"]);
-					break;
-				case 1:
-					hostname = args[0];
-					port = 5555;
-					includeGCStats = true;
-					break;
-				case 2:
-					hostname = args[0];
-					if (!ushort.TryParse(args[1], out port)) {
-						Usage();
-						Environment.Exit(-1);
-					}
-					includeGCStats = true;
-					break;
-				default:
-					Usage();
-					Environment.Exit(-1);
-					return;
-			}
-			var client = new Client(hostname, port);
+            var appSettings = ConfigurationManager.AppSettings;
+            var hostname = appSettings["RiemannHost"];
+            var port = UInt16.Parse(appSettings["RiemannPort"]);
+            var includeGCStats = Boolean.Parse(appSettings["IncludeGCstats"]);
 
-			var reporters = Health.Reporters(includeGCStats)
-				.ToList();
-			while (true) {
-				foreach (var reporter in reporters) {
-					string description;
-					float value;
-
-					if (reporter.TryGetValue(out description, out value)) {
-						string state;
-						if (value < reporter.WarnThreshold) {
-							state = "ok";
-						} else if (value < reporter.CriticalThreshold) {
-							state = "warning";
-						} else {
-							state = "critical";
-						}
-						client.SendEvent(reporter.Name, state, description, value, 1);
-					}
-				}
-				Thread.Sleep(TimeSpan.FromSeconds(1.0));
-			}
-		}
-
-
-		private static void Usage() {
-			Console.WriteLine(@"riemann [[riemann-host] [riemann-port]]
-If not including the host and port, please modify the App.config to suit your needs.");
+		    HostFactory.Run(x =>
+		        {
+		            x.Service<HealthService>(s =>
+		                {
+		                    s.ConstructUsing(_ => new HealthService(hostname, port, includeGCStats));
+		                    s.WhenStarted(tc => tc.Start());
+		                    s.WhenStopped(tc => tc.Stop());
+		                });
+		            x.RunAsLocalSystem();
+                    x.SetDescription("Riemann Health Monitor");
+                    x.SetDisplayName("RiemannHealth");
+                    x.SetServiceName("RiemannHealth");
+		        });
 		}
 	}
 }
